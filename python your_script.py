041c36aa21820e7871a759c542ee25fed4e727c3
That is me! <pallavisathya13@gmail.com>
2024-12-29 01:47:03 +0530

    Updated script or changes

---
+++ python your_script.py
@@ -0,0 +1,17 @@
+from tpot import TPOTClassifier
+from sklearn.datasets import load_digits
+from sklearn.model_section import train_test_split
+
+
+digits = load_digits()
+X_train, X_test, y_train, y_test = train_test_split(digits.data, digits.target, train_size=0.75)
+
+tpot = TPOTClassifier(verbosity=2)
+tpot.fit(X_train, y_train)
+
+print(tpot.score(X_test, y_test))
+
+
+
+
+
